7d028ce60673e44d282089ffb153272d3bbec9fb
Rupa-421 <rupasrikkd147@gmail.com>
2021-10-02 16:36:49 +0530

    edit

--- kidzgeometry/settings.py
+++ kidzgeometry/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-g*r-_#a8l$z@-pbx_^xq-et4#&3+@ezgs6o_ocrl(2fchh)r53'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['kidzgeometry.herokuapp.com', 'localhost', '127.0.0.1']
 
